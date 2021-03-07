07a9bcdf00e82ed785b968d21b7e482191b43525
skarab42 <skarab@onlfait.ch>
2021-03-07 10:15:13 +0100

    Ajoutez des fichiers projet.

---
+++ ValheimWebSocket/ValheimWebSocketPlugin.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+
+namespace ValheimWebSocket
+{
+
+    [BepInPlugin(pluginGUID, pluginName, pluginVersion)]
+    public class ValheimWebSocketPlugin : BaseUnityPlugin
+    {
+        public const string pluginGUID = "org.bepinex.plugins.valheim_websocket";
+        public const string pluginName = "Valheim WebSocket";
+        public const string pluginVersion = "0.1.0";
+
+        void Awake()
+        {
+            UnityEngine.Debug.Log($"{pluginName} v{pluginVersion}");
+        }
+    }
+}
